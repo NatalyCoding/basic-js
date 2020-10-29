38d82202dcf6984045e15b7c242c4ef52fa94e54
Nataly Coding <natalycoding@gmail.com>
2020-10-29 23:05:42 +0200

    feat: what season task solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date === undefined) {
+        return 'Unable to determine the time of year!';
+    }
+
+    if (Object.prototype.toString.call(date) !== '[object Date]') {
+        throw new Error('Wrong date');
+    }
+
+    if (date.getMonth) {
+        let month = date.getMonth();
+        return month <= 1 ? 'winter' :
+            month <= 4 ? 'spring' :
+            month <= 7 ? 'summer' :
+            month <= 10 ? 'autumn' :
+            'winter';
+    };
+
+    throw new Error('Wrong date');
 };
